eb6206c3dd3d797ad14b6b9e60c6458a616f44ed
wgbx <wgbx@foxmail.com>
2025-05-09 17:26:16 +0800

    feat: 添加 Tailwind CSS 配置及依赖

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="App">
       <Button type="primary">Button</Button>
+      <h1 className="flex flex-col items-center justify-between min-h-screen p-24">Hello world!</h1>
     </div>
   )
 }
